fca9eadefe136c5a387b8e09c48e39fd1723d537
Matthew <matthew.becknell91@gmail.com>
2025-06-13 01:58:38 -0400

    Update: Cleaned settings, added media support, polished templates.

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +25,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6iy(q#l61f9--sx+&^useeq!#_e4d#pyqmi&r&ew1xk0rzu#54'
-
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-local-debug-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -110,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization

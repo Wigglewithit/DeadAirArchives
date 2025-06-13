0815c450d4360a5a25ec2ede76591231f48aa6e2
Matthew <matthew.becknell91@gmail.com>
2025-06-13 06:01:56 -0400

    Fixed Security Issues(Novice problems.).. =)

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-local-debug-key")
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 

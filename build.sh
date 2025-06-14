83c6c684a16ecc7cece8c4f2f193a13fb71adc15
Matthew <matthew.becknell91@gmail.com>
2025-06-14 17:04:25 -0400

    Fix: use custom build script for DB readiness

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit  # Exit on error
 
-# Install dependencies
-pip install -r requirements.txt
+# Wait until the database is ready
+echo "Waiting for Postgres to be available..."
+while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+  sleep 1
+done
 
-# Run Django management commands
-python manage.py collectstatic --no-input
+echo "Postgres is available, continuing..."
+
+# Run migrations
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
